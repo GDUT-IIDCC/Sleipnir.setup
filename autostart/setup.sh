def2dc122c016794ffb33a268cd0b47e6af472c7
spongebob <spongebob@mail2.gdut.edu.cn>
2021-05-29 18:00:50 +0800

    [bugfix] 解决can自启动失效的bug.PS：After字段只起描述性作用，Requires才起理论上需要的依赖作用

--- autostart/setup.sh
+++ autostart/setup.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
-sudo gs_usb >> /etc/modules
+sudo bash -c "echo gs_usb >> /etc/modules"
 sudo cp *.service /etc/systemd/system/
 sudo cp panel.desktop ~/Desktop
-chmod +x ~/Desktop/panel.desktop
+sudo chmod +x ~/Desktop/panel.desktop
 cp launch_ui.sh ~/
-sudo cp s.png /usr/share/icons/hicolor/512x512/apps/sleipnir.png
+sudo cp spongebob.png /usr/share/icons/hicolor/512x512/apps/spongebob.png
 sudo systemctl daemon-reload
 sudo systemctl enable can.service 
-sudo systemctl enable roscore.service
+# 暂时不启用后台roscore
+# sudo systemctl enable roscore.service
