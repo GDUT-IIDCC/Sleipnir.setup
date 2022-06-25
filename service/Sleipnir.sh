d658ce686df5747e8644e07171c45618bdc5cf06
Natsu-Akatsuki <1024756993@qq.com>
2022-06-25 10:57:45 +0800

    docs: 修正icon和服务

--- service/Sleipnir.sh
+++ service/Sleipnir.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 log_dir="${HOME}/log.Sleipnir"
-file_dir="${log_dir}/${time_prefix}-debug.log"
 time_prefix=$(date '+%m-%d-%H-%M')
-conda activate sleipnir
+file_dir="${log_dir}/${time_prefix}-debug.log"
+
 source ~/Sleipnir/devel/setup.bash
+
 # 若不存在，则建日志文件夹
 test -d ${log_dir} || mkdir ${log_dir}
 echo -e "\e[32m日志信息将保存到${file_dir}下\e[0m" | cowthink -f duck
